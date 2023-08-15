06057d2399bafa08d6b50664e4f6f589b789324a
thepolkamonster <sakshamparashar12@gmail.com>
2023-08-15 07:35:30 +0530

    Modified host and port in app.py

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ app.register_blueprint(accountSettingsBlueprint)
 app.register_blueprint(adminPanelCommentsBlueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True, host=socket.gethostbyname(socket.gethostname()))
+    #app.run(debug=True, host=socket.gethostbyname(socket.gethostname()))
+    app.run(host = "0.0.0.0", port = 5000)
